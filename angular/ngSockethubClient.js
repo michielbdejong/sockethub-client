6201c68ee19e9dae72415624659e3d16734d9e06
Nick Jennings <nick@silverbucket.net>
2013-06-25 00:01:30 +0200

    added config directive and reworked the settings methods

--- angular/ngSockethubClient.js
+++ angular/ngSockethubClient.js
@@ -5,45 +5,30 @@ angular.module('ngSockethubClient', []).
  * default settings
  */
 value('settings', {
-  sockethub: function () {
-    // figure out sockethub connect settings
-    var settings = {
-      host: 'localhost',
-      port: '10550',
-      path: '/sockethub',
-      tls: false,
-      secret: '1234567890'
-    };
-
-    return settings;
-  }
-}).
-
-
-/**
- * factory: SH
- */
-factory('SH', ['$rootScope', '$q', '$timeout', 'settings',
-function ($rootScope, $q, $timeout, settings) {
-
-  var sc;
-  var config = settings;
-  var callbacks = {
-    'error': {},
-    'message': {},
-    'response': {},
-    'close': {}
-  };
-
-
-  function existsConfig() {
-    return verifyConfig(config);
-  }
-
-
-  function verifyConfig(p) {
+  conn: {
+    host: 'localhost',
+    port: '10550',
+    path: '/sockethub',
+    tls: false,
+    secret: '1234567890'
+  },
+  env: {
+    logo: '/res/img/sockethub-logo.svg'
+  },
+  save: function (prop, obj) {
+    if (this.verify(prop, obj)) {
+      this[prop] = obj;
+      return true;
+    } else {
+      return false;
+    }
+  },
+  exists: function (prop) {
+    this.verify(prop, config);
+  },
+  verify: function (prop, p) {
     if (!p) {
-      p = config;
+      p = this[prop];
     }
     if ((p.host) && (p.host !== '') &&
         (p.port) && (p.port !== '') &&
@@ -55,22 +40,22 @@ function ($rootScope, $q, $timeout, settings) {
       return false;
     }
   }
+}).
 
 
-  function setConfig(p) {
-    var defer = $q.defer();
-    console.log('SH.setConfig: received ' + p.host + ', ' + p.port + ', ' +
-                                     p.path + ', TLS:' + p.tls +
-                                     ', SECRET:' + p.secret);
-    if (verifyConfig(p)) {
-      config = p;
-      defer.resolve();
-    } else {
-      defer.reject('some config properties missing');
-    }
-    return defer.promise;
-  }
-
+/**
+ * factory: SH
+ */
+factory('SH', ['$rootScope', '$q', '$timeout', 'settings',
+function ($rootScope, $q, $timeout, settings) {
+  var sc;
+  var config = settings.conn;
+  var callbacks = {
+    'error': {},
+    'message': {},
+    'response': {},
+    'close': {}
+  };
 
   function isConnected() {
     if (sc) {
@@ -80,7 +65,6 @@ function ($rootScope, $q, $timeout, settings) {
     }
   }
 
-
   function isRegistered() {
     if (sc) {
       return sc.isRegistered();
@@ -88,7 +72,6 @@ function ($rootScope, $q, $timeout, settings) {
       return false;
     }
   }
-
 
   function callOnReady(p) {
     var defer = $q.defer();
@@ -113,15 +96,14 @@ function ($rootScope, $q, $timeout, settings) {
     return defer.promise;
   }
 
-
   function connect() {
     var defer = $q.defer();
     var scheme = 'ws://';
-    if (config.tls) {
+    if (settings.conn.tls) {
       scheme = 'wss://';
     }
     SockethubClient.connect({
-      host: scheme + config.host + ':' + config.port + config.path,
+      host: scheme + settings.conn.host + ':' + settings.conn.port + settings.conn.path,
       confirmationTimeout: 3000   // timeout in miliseconds to wait for confirm
     }).then(function (connection) {
       sc = connection;
@@ -169,7 +151,6 @@ function ($rootScope, $q, $timeout, settings) {
     return defer.promise;
   }
 
-
   function register() {
     var defer = $q.defer();
     console.log('SH.register() called');
@@ -177,12 +158,11 @@ function ($rootScope, $q, $timeout, settings) {
       testFunc: isConnected,
       callFunc: 'register',
       callParams: [{
-        secret: config.secret
+        secret: settings.conn.secret
       }]
     }).then(defer.resolve, defer.reject);
     return defer.promise;
   }
-
 
   function sendSet(platform, type, index, object) {
     var defer = $q.defer();
@@ -199,7 +179,6 @@ function ($rootScope, $q, $timeout, settings) {
     return defer.promise;
   }
 
-
   function sendSubmit(obj, timeout) {
     var defer = $q.defer();
 
@@ -212,16 +191,11 @@ function ($rootScope, $q, $timeout, settings) {
     return defer.promise;
   }
 
-
   var on = function on(platform, type, func) {
     callbacks[type][platform] = func;
   };
 
-
   return {
-    config: config,
-    existsConfig: existsConfig,
-    setConfig: setConfig,
     connect: connect,
     register: register,
     isConnected: isConnected,
@@ -229,5 +203,110 @@ function ($rootScope, $q, $timeout, settings) {
     set: sendSet,
     submit: sendSubmit,
     on: on
+  };
+}]).
+
+
+/**
+ * emitters
+ */
+run(['$rootScope',
+function ($rootScope) {
+  $rootScope.$on('showModalSockethubSettings', function(event, args) {
+    backdrop_setting = true;
+    if ((typeof args === 'object') && (typeof args.locked !== 'undefined')) {
+      if (args.locked) {
+        backdrop_setting = "static";
+      }
+    }
+    $("#modalSockethubSettings").modal({
+      show: true,
+      keyboard: true,
+      backdrop: backdrop_setting
+    });
+  });
+
+  $rootScope.$on('closeModalSockethubSettings', function(event, args) {
+    $("#modalSockethubSettings").modal('hide');
+  });
+}]).
+
+
+directive('sockethubSettings', ['SH', 'settings', '$rootScope',
+function (SH, settings, $rootScope) {
+  return {
+    restrict: 'A',
+    template: '<div id="modalSockethubSettings" class="modal hide fade">' +
+              '  <div class="modal-header">' +
+              '    <img src="{{ settings.env.logo }}" width="200" />' +
+              '  </div>' +
+              '  <div class="modal-body">' +
+              '    <p>In order to connect to Sockethub, please enter the host, port and secret provided to you from your service provider</p>' +
+              '    <form name="settingsSockethub" class="form-horizontal" novalidate>' +
+              '      <fieldset>' +
+              '        <div class="control-group">' +
+              '          <label for="host" class="control-label">Hostname</label>' +
+              '          <div class="controls">' +
+              '            <input type="text" class="required input-large" name="host" placeholder="Enter hostname..." ng-model="settings.conn.host" required>' +
+              '          </div>' +
+              '        </div>' +
+              '        <div class="control-group">' +
+              '          <label for="port" class="control-label">Port</label>' +
+              '          <div class="controls">' +
+              '            <input type="text" class="required input-small" name="port" placeholder="Enter port..." ng-model="settings.conn.port" required>' +
+              '           </div>' +
+              '         </div>' +
+              '        <div class="control-group">' +
+              '          <label for="secret" class="control-label">Secret</label>' +
+              '          <div class="controls">' +
+              '            <input type="text" class="required input-xlarge" name="secret" placeholder="Enter secret..." ng-model="settings.conn.secret" required>' +
+              '          </div>' +
+              '        </div>' +
+              '        <div class="control-group">' +
+              '          <div class="controls">' +
+              '            <button ng-click="save(settings.conn)" ng-disabled="!settings.verify(\'conn\', settings.conn) || saving">Submit</button>' +
+              '          </div>' +
+              '        </div>' +
+              '      </fieldset>' +
+              '    </form>' +
+              '    <p></p>' +
+              '    <div class="message-container">' +
+              '      <div message></div>' +
+              '    </div>' +
+              '  </div>' +
+              '  <div class="modal-footer">' +
+              '  </div>' +
+              '</div>',
+    link: function (scope) {
+      console.log('SH SETTINGS: ', settings);
+      scope.saving = false;
+      scope.settings = settings;
+      scope.save = function (cfg) {
+        scope.saving = true;
+        $rootScope.$broadcast('message', {
+              type: 'clear',
+        });
+        settings.save('conn', cfg);
+        SH.connect().then(function () {
+          return SH.register();
+        }).then(function () {
+          scope.saving = false;
+          console.log('connected to sockethub');
+          $rootScope.$broadcast('message', {
+                message: 'connected to sockethub',
+                type: 'success',
+                timeout: false
+          });
+        }, function (err) {
+          scope.saving = false;
+          console.log('error connection to sockethub: ', err);
+          $rootScope.$broadcast('message', {
+                message: err,
+                type: 'error',
+                timeout: false
+          });
+        });
+      };
+    }
   };
 }]);
