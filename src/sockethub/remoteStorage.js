ea146ebf146d6663cf56aba6e20dc93013d5ffb2
() <nilclass@riseup.net>
2013-06-28 14:52:11 +0200

    added copyright header to all source files (fixes #12)

--- src/sockethub/remoteStorage.js
+++ src/sockethub/remoteStorage.js
@@ -1,3 +1,23 @@
+/**
+ * This file is part of sockethub-client.
+ *
+ * © 2013 Niklas E. Cathor (https://github.com/nilclass)
+ * © 2013 Nick Jennings (https://github.com/silverbucket)
+ *
+ * sockethub-client is dual-licensed under either the MIT License or GPLv3 (at your choice).
+ * See the files LICENSE-MIT and LICENSE-GPL for details.
+ *
+ * The latest version of sockethub-client can be found here:
+ *   git://github.com/sockethub/sockethub-client.git
+ *
+ * For more information about sockethub visit http://sockethub.org/.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ */
+
 define([], function() {
 
   var module = function(privateClient, publicClient) {
