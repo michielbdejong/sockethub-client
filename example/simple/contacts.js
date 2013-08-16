70f963cdb4adfdda0ecc6e3e5b26a8d6e663d9c0
Michiel de Jong <michiel@unhosted.org>
2013-08-16 13:47:53 +0200

    simple example

---
+++ example/simple/contacts.js
@@ -0,0 +1,17 @@
+var contacts = {
+  'Twitter (my public stream)': {
+    platform: 'twitter',
+    verb: 'post',
+    target: 'me'
+  },
+  'Facebook (my public stream)': {
+    platform: 'facebook',
+    verb: 'post',
+    target: 'me'
+  },
+  'user@example.com (email)': {
+    platform: 'email',
+    verb: 'send',
+    target: 'user@example.com'
+  }
+};
